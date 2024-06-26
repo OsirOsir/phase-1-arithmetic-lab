97b96f621d045098621bd64dc1c11a41abfa2d29
Philip osir Otieno <philiposir2@gmail.com>
2024-06-26 07:10:21 +0300

    arithmetic values

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+
+const num3 = 11;
+const num4 = 7;
+const mod = num3 % num4;
+
+const max = Math.max(13, 15, 18, 20);
